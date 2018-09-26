397e57aea381ef6c6a2dd5f4806d793ff24d079e
Geir Ulvik <ulvik@usit.uio.no>
2018-09-26 19:07:22 +0200

    Added tests for most functions. Linreg keep making coeffs ~0 for terms of degree > 1.

--- oblig1/analysis.py
+++ oblig1/analysis.py
@@ -2,12 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Function definitions
-def vandermonde_matrix(x_data):
+def vandermonde_matrix(data, m):
     """Generate the vendermonde matrix associated with the data set"""
-    N = len(x_data)
-    A = np.ones((N,N))
-    for i in range(1,N):
-        A[i] = x_data**i
+    N = len(data)
+    A = np.zeros((N,m+1))
+    A[:,0] = 1.0
+    for i in range(1,m+1):
+        A[:,i] = data**i
     return A
 
 def back_substitution(mat_A, b):
@@ -17,12 +18,12 @@ def back_substitution(mat_A, b):
     """
     N = len(mat_A[0])
     x = np.zeros(N)
-    x[N-1] = b[N-1]/mat_A[N-1,N-1]
-    for i in range(2, N):
+    for i in reversed(range(N)):
         sum = 0
-        for j in range(i+1, N):
-            sum += mat_A[N-i,j]*x[N-j]
-        x[N-i] = (b[N-i] - sum)/mat_A[N-i,N-i]
+        for j in range(i+1,N):
+            sum += mat_A[i,j]*x[j]
+        print("SUM = ", sum)
+        x[i] = (b[i] - sum)/mat_A[i,i]
     return x
 
 def forward_substitution(mat_A, b):
@@ -32,19 +33,18 @@ def forward_substitution(mat_A, b):
     """
     N = len(mat_A[0])
     x = np.zeros(N)
-    x[0] = b[0]/a[0,0]
-    for i in range(1, N):
+    for i in range(N):
         sum = 0
-        for j in range(i-1):
+        for j in range(i):
             sum += mat_A[i,j]*x[j]
         x[i] = (b[i] - sum)/mat_A[i,i]
     return x
 
-def find_coeffs_from_data(data):
+def find_coeffs_from_data(data, m):
     """Find the coefficients for a smooth function fitting."""
-    A = vandermonde_matrix(data)
+    A = vandermonde_matrix(data, m)
     q,r = np.linalg.qr(A)
-    coeffs = back_substitution(r, data)
+    coeffs = back_substitution(r, q.T.dot(data))
     return coeffs
 
 def polynomial_degree_m(x, coeffs, m):
@@ -56,37 +56,44 @@ def polynomial_degree_m(x, coeffs, m):
     N = len(x)
     polynomial = np.zeros(N)
     for i in range(N):
-        for j in range(m):
+        for j in range(m+1):
             polynomial[i] += coeffs[j]*x[i]**j
     return polynomial
 
-# Datasets as defined from generate_data.py
-n = 30
-start = -2
-stop = 2
-x = np.linspace(start, stop, n)
-eps = 1
-np.random.seed(1) # use same seed every time
-r = np.random.random(n) * eps
-data_one = x*(np.cos(r + 0.5*x**3) + np.sin(0.5*x**3))
-data_two = 4*x**5 - 5*x**4 - 20*x**3 + 10*x*x + 40*x + r
+if __name__ == '__main__':
+    # Datasets as defined from generate_data.py
+    n = 30
+    start = -2.0
+    stop = 2.0
+    x = np.linspace(start, stop, n)
+    eps = 1.0
+    np.random.seed(1) # use same seed every time
+    r = np.random.random(n) * eps
+    data_one = x*(np.cos(r + 0.5*x**3) + np.sin(0.5*x**3))
+    data_two = 4*x**5 - 5*x**4 - 20*x**3 + 10*x*x + 40*x + r
 
-m = 3 # degree of p(x)
+    m = 5 # degree of p(x)
 
-# Find coefficients
-coeffs_one = find_coeffs_from_data(data_one)
-print(coeffs_one)
-coeffs_two = find_coeffs_from_data(data_two)
+    # Find coefficients
+    coeffs_one = find_coeffs_from_data(data_one, m)
+    #print(coeffs_one)
+    #coeffs_two = find_coeffs_from_data(data_two, m)
+    #print(coeffs_two)
 
-# Plotting
-plt.subplot(1,2,1)
-plt.plot(x, data_one, 'o')
-plt.plot(x, polynomial_degree_m(x, coeffs_one, m), 'r', label='Fit')
-plt.legend()
 
-plt.subplot(1,2,2)
-plt.plot(x, data_two, 'o')
-plt.plot(x, polynomial_degree_m(x, coeffs_two, m), 'r', label='Fit')
-plt.legend()
+    # Generate polynomial fits.
+    poly_one = polynomial_degree_m(x, coeffs_one, m)
+    #poly_two = polynomial_degree_m(x, coeffs_two, m)
 
-plt.show()
+    # Plotting
+    plt.subplot(1,2,1)
+    plt.plot(x, data_one, 'o')
+    plt.plot(x, poly_one, 'r', label='Fit')
+    plt.legend()
+
+    plt.subplot(1,2,2)
+    plt.plot(x, data_two, 'o')
+    #plt.plot(x, poly_two, 'r', label='Fit')
+    plt.legend()
+
+    plt.show()
--- oblig1/tests.py
+++ oblig1/tests.py
@@ -1,2 +1,43 @@
 import numpy as np
+import unittest
 from analysis import *
+
+# Test variables
+x_in = np.array([1,2,3,4])
+A_upper = np.c_[[1,0,0], [2,2,0], [3,3,3]]
+A_lower = np.c_[[3,3,3], [0,2,2], [0,0,1]]
+b_upper = np.array([4,1,3])
+b_lower = np.array([3,-1,1])
+coeffs = np.array([1,2,3])
+m = 2
+
+
+
+class TestLinregMethods(unittest.TestCase):
+
+    def test_vandermonde_matrix(self):
+        """Test for the vandermonde function"""
+        expected_A = np.c_[[1,1,1,1], [1,2,3,4], [1,4,9,16]].astype(float)
+        computed_A = vandermonde_matrix(x_in, m)
+        self.assertTrue(np.array_equal(expected_A, computed_A))
+
+    def test_back_substitution(self):
+        """Test for the back substitution algorithm"""
+        expected_x = np.array([3,-1,1], dtype=float)
+        computed_x = back_substitution(A_upper, b_upper)
+        self.assertTrue(np.array_equal(expected_x, computed_x))
+
+    def test_forward_substitution(self):
+        """Test for the back substitution algorithm"""
+        expected_x = np.array([1,-2,2], dtype=float)
+        computed_x = forward_substitution(A_lower, b_lower)
+        self.assertTrue(np.array_equal(expected_x, computed_x))
+
+    def test_polynomial_degree_m(self):
+        """Test that the polynomial_degree_m function returns polynomials."""
+        expected_p = np.array([6,17,34,57], dtype=float)
+        computed_p = polynomial_degree_m(x_in, coeffs, m)
+        self.assertTrue(np.array_equal(expected_p, computed_p))
+
+if __name__ == '__main__':
+    unittest.main()
