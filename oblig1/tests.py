fdcc03c315810c341ff81f592e407f90a1c11630
Geir Ulvik <ulvik@usit.uio.no>
2018-09-26 21:36:31 +0200

    Removed redundant docstrings in tests.py

--- oblig1/tests.py
+++ oblig1/tests.py
@@ -16,31 +16,26 @@ m = 2
 class TestLinregMethods(unittest.TestCase):
 
     def test_vandermonde_matrix(self):
-        """Test for the vandermonde function"""
         expected_A = np.c_[[1,1,1,1], [1,2,3,4], [1,4,9,16]].astype(float)
         computed_A = vandermonde_matrix(x_in, m)
         self.assertTrue(np.array_equal(expected_A, computed_A))
 
     def test_back_substitution(self):
-        """Test for the back substitution algorithm"""
         expected_x = np.array([3,-1,1], dtype=float)
         computed_x = back_substitution(A_upper, b_upper)
         self.assertTrue(np.array_equal(expected_x, computed_x))
 
     def test_forward_substitution(self):
-        """Test for the back substitution algorithm"""
         expected_x = np.array([1,-2,2], dtype=float)
         computed_x = forward_substitution(A_lower, b_lower)
         self.assertTrue(np.array_equal(expected_x, computed_x))
 
     def test_polynomial_degree_m(self):
-        """Test that the polynomial_degree_m function returns polynomials."""
         expected_p = np.array([6,17,34,57], dtype=float)
         computed_p = polynomial_degree_m(x_in, coeffs, m)
         self.assertTrue(np.array_equal(expected_p, computed_p))
 
     def test_cholesky_factorization(self):
-        """Test that the cholesky factorization algorithm does the thing."""
         testmat = np.c_[[3,4], [4,6]]
         exp_L = np.c_[[1.0,4/3],[0,1.0]]
         exp_D = np.c_[[3.0,0],[0,2/3]]
